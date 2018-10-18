b72f66fa462aa4ba42219be7a1c540c4bc7277b6
Blizrod <blizrod11@gmail.com>
2018-10-18 11:09:35 +0300

    Прошли типы переменных и значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String somebody = "Rodion";
+    System.out.println("Hello, " + somebody + "!");
+
+    double l = 10;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 
 }
